51100cf751ba12f110b17406fbb84ce9b02dd7c7
robertabononi <roberta.bononi16@gmail.com>
2022-06-17 14:14:53 -0300

    refactor: controlando as notícias do blog com js

--- js/carousel.js
+++ js/carousel.js
@@ -1,9 +1,39 @@
+//Variáveis
 var btns = document.querySelectorAll('.listaDeArtigos-slider-item');
+var new0 = document.querySelector('#new0');
+var new1 = document.querySelector('#new1');
+var new2 = document.querySelector('#new2');
+
+new0.style.display = 'block';
+
+//Criando indicador de slide atual
+var indicadorSlideAtual = document.createElement('span');
+indicadorSlideAtual.classList.add('escondeVisualmente');
+indicadorSlideAtual.id ='escondeVisualmente';
+indicadorSlideAtual.textContent = '(Slide atual)';
 
 // Percorre todos os botoes controladores
 btns.forEach(function(btn) {
   btn.addEventListener('click', function() {
-    
+
+    btn.href = 'javascript:voide(0)'; //Evita o deslocamento da tela ao clicar em alguma notícia
+
+    if (this.getAttribute('data-sliderItem') === '0') {
+      new0.style.display = 'block';
+      new1.style.display = 'none';
+      new2.style.display = 'none';
+    } else if (this.getAttribute('data-sliderItem') === '1') {
+      new0.style.display = 'none';
+      new1.style.display = 'block';
+      new2.style.display = 'none';
+    } else {
+      new0.style.display = 'none';
+      new1.style.display = 'none';
+      new2.style.display = 'block';
+    }
+
+    document.querySelector('.listaDeArtigos-slider-item .escondeVisualmente').remove();
+    this.append(indicadorSlideAtual);
 
     // Remove classe 'ativo' dos outros botoes
     btns.forEach(function(btnRemoveClass) {
